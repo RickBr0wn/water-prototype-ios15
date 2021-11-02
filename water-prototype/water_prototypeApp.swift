c88a379e7f136b1dd4dc9ed41b7132d76d4c6508
Rick Brown <ricky.brown.00@gmail.com>
2021-11-02 22:28:44 +0000

    Initial Commit

---
+++ water-prototype/water_prototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  water_prototypeApp.swift
+//  water-prototype
+//
+//  Created by Rick Brown on 02/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct water_prototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
